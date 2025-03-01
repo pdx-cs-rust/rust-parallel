75e0ad226f0f458f25e44994b335a2476197d4dd
Bart Massey <bart.massey@gmail.com>
2025-02-28 18:33:18 -0800

    moved to ndarray

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
 use std::ops::Range;
 
-use num::{Complex};
+use ndarray::Array2;
+use num::Complex;
 #[cfg(feature = "rayon")]
 use rayon::prelude::*;
 
-const WIDTH: usize = 8000;
-const HEIGHT: usize = 1000;
-
 #[cfg(feature = "rayon")]
 macro_rules! gen_iter_mut {
-    ($r:expr) => { $r.par_iter_mut() };
+    ($r:expr) => { $r.par_bridge() };
 }
+
 #[cfg(not(feature = "rayon"))]
 macro_rules! gen_iter_mut {
-    ($r:expr) => { $r.iter_mut() };
+    ($r:expr) => { $r };
 }
 
 type C = Complex<f64>;
 
-fn escapes(bound: usize, c: C) -> Option<usize> {
+fn escapes(bound: u16, c: C) -> Option<u16> {
     let mut z = Complex::new(0.0, 0.0);
-    for g in 0..=bound {
+    for g in 0..bound {
         if z.norm_sqr() > 4.0 {
             return Some(g)
         }
@@ -38,28 +37,25 @@ fn test_escapes() {
 }
 
 type R = Range<f64>;
-type Image = [[usize; WIDTH]; HEIGHT];
 
-
-fn mandelbrot(bound: usize, (xz, yz): (R, R)) -> Box<Image>
-{
-    let mut result: Box<Image> = Box::new([[0; WIDTH]; HEIGHT]);
-    let x_step = (xz.end - xz.start) / WIDTH as f64;
-    let y_step = (yz.end - yz.start) / HEIGHT as f64;
-    // Thanks to DeepSeek for help with the rayon.
-    gen_iter_mut!(result).enumerate().for_each(move |(j, row)| {
+fn mandelbrot(
+    bound: u16,
+    (width, height): (usize, usize),
+    (xz, yz): (R, R),
+) -> Array2<u16> {
+    let mut result = Array2::zeros((width, height));
+    let x_step = (xz.end - xz.start) / width as f64;
+    let y_step = (yz.end - yz.start) / width as f64;
+    gen_iter_mut!(result.indexed_iter_mut()).for_each(move |((i, j), v)| {
         let y = yz.start + j as f64 * y_step;
-        #[allow(clippy::needless_range_loop)]
-        gen_iter_mut!(row).enumerate().for_each(move |(i, v)| {
-            let x = xz.start + i as f64 * x_step;
-            *v = escapes(bound, Complex::new(x, y)).unwrap_or(bound + 1);
-        });
+        let x = xz.start + i as f64 * x_step;
+        *v = escapes(bound, Complex::new(x, y)).unwrap_or(bound + 1);
     });
     result
 }
 
-fn display(a: &Image) {
-    for row in a {
+fn display(a: &Array2<u16>) {
+    for row in a.rows() {
         for v in row {
             let c = match v {
                 0..10 => ' ',
@@ -74,11 +70,13 @@ fn display(a: &Image) {
 }
 
 fn main() {
-    let ratio = WIDTH as f64 / HEIGHT as f64;
+    let width = 8000;
+    let height = 1000;
+    let ratio = width as f64 / height as f64;
     let r = (
         (-1.0 * ratio..1.0 * ratio),
         (-1.0..1.0),
     );
-    let m = mandelbrot(255, r);
+    let m = mandelbrot(255, (width, height), r);
     display(&m);
 }
